79664b0af7d38d7b8222e92b7e43af523496a75a
umair <muhammadumair2511@gmail.com>
2023-07-20 13:02:21 +0500

    nav bar and hero section responsivness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import Logo from "../assets/logo.svg";
 import Menu from "../assets/hamburger.svg";
+import Close from "../assets/close.svg";
 import "../styles/Navbar.css";
 
 const Navbar = ({
@@ -9,19 +11,59 @@ const Navbar = ({
   projectsRef,
   contactRef,
 }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className="navbar-container">
-      <img src={Logo} alt="umair" />
-      {window.innerWidth >= 768 && (
-        <ul className="nav-menu">
-          <li onClick={() => scrollToRef(aboutRef)}>About</li>
-          <li onClick={() => scrollToRef(technologiesRef)}>Technologies</li>
-          <li onClick={() => scrollToRef(projectsRef)}>Projects</li>
-          <li onClick={() => scrollToRef(contactRef)}>Contact</li>
-        </ul>
+      <img
+        src={Logo}
+        alt="umair"
+        onClick={() =>
+          window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+          })
+        }
+      />
+      <div
+        className={
+          window.innerWidth <= 768 && menuOpen ? "overlay active" : "overlay"
+        }
+      ></div>
+      {window.innerWidth <= 768 && (
+        <div
+          className={
+            window.innerWidth <= 768 && menuOpen
+              ? "nav-menu-container active"
+              : "nav-menu-container"
+          }
+        >
+          <ul className="nav-menu">
+            <li onClick={() => scrollToRef(aboutRef)}>About</li>
+            <li onClick={() => scrollToRef(technologiesRef)}>Technologies</li>
+            <li onClick={() => scrollToRef(projectsRef)}>Projects</li>
+            <li onClick={() => scrollToRef(contactRef)}>Contact</li>
+          </ul>
+        </div>
+      )}
+      {window.innerWidth > 768 && (
+        <div className="nav-menu-container">
+          <ul className="nav-menu">
+            <li onClick={() => scrollToRef(aboutRef)}>About</li>
+            <li onClick={() => scrollToRef(technologiesRef)}>Technologies</li>
+            <li onClick={() => scrollToRef(projectsRef)}>Projects</li>
+            <li onClick={() => scrollToRef(contactRef)}>Contact</li>
+          </ul>
+        </div>
       )}
 
-      {window.innerWidth <= 768 && <img src={Menu} alt="menu" />}
+      {window.innerWidth <= 768 && (
+        <img
+          src={menuOpen ? Close : Menu}
+          alt="menu"
+          onClick={() => setMenuOpen((prevState) => !prevState)}
+          className="nav-button"
+        />
+      )}
     </nav>
   );
 };
