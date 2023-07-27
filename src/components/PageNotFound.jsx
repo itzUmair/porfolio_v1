886554195702dc706382e192d70036dfb49bb83e
umair <muhammadumair2511@gmail.com>
2023-07-27 12:16:37 +0500

    added 404 page

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,5 +1,26 @@
+import NotFound from "../assets/404.svg";
+import "../styles/PageNotFound.css";
+
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="not-found-container">
+      <img src={NotFound} alt="" />
+      <h2>Looks like you got lost!</h2>
+      <p className="current-url">
+        You reached: {window.location.href}, which does not exist.
+      </p>
+      <p> Did you mean to visit:</p>
+      <a href="https://muhammadumairkhan.vercel.app">
+        muhammadumairkhan.vercel.app
+      </a>
+      <a href="https://muhammadumairkhan.vercel.app/resume">
+        muhammadumairkhan.vercel.app/resume
+      </a>
+      <a href="https://muhammadumairkhan.vercel.app/projects">
+        muhammadumairkhan.vercel.app/projects
+      </a>
+    </div>
+  );
 };
 
 export default PageNotFound;
